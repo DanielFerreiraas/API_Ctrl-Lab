f13fd5e14bac11514c81b9e062d4ec80da523cc5
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-09-26 20:13:36 -0300

    Camada controller de User finalizada

---
+++ src/modules/auth/user/api/controllers/impl/userControllerImpl.ts
@@ -0,0 +1,16 @@
+import { BaseControllerImpl } from "@/modules/base/BaseControllerimpl";
+import { CreateUserDTO, UpdateUserDTO, UserDTO } from "../../../dtos/UserDTO";
+import { inject, injectable } from "tsyringe";
+import { UserController } from "../userController";
+import { UserService } from "../../../business/services/UserService";
+
+@injectable()
+export class UserControllerImpl extends BaseControllerImpl<UserDTO, CreateUserDTO, UpdateUserDTO> {
+
+    constructor(
+        @inject("UserService")
+        userService: UserService
+    ){
+        super(userService)
+    }
+}
