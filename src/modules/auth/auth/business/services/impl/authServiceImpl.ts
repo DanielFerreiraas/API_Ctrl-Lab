aafd6907ceec1b7b7af59a462d4b2e569b6b01e1
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-10-05 01:41:08 -0300

    faixada de senha

--- src/modules/auth/auth/business/services/impl/authServiceImpl.ts
+++ src/modules/auth/auth/business/services/impl/authServiceImpl.ts
@@ -3,6 +3,7 @@ import { AuthHelpers } from '@/config/hash/hash';
 
 import { authDTO, signupDTO, tokenDTO } from '../../../dtos/authDTO';
 import { AuthService } from '../authService';
+import passwordFacade from '../../facedes/PasswordFacade';
 import { UserService } from '@/modules/auth/user/business/services/UserService';
 import { sign } from 'jsonwebtoken';
 import JWTFacade from '@/shared/facades/TokenFacade';
@@ -29,6 +30,7 @@ export class AuthServiceImpl implements AuthService {
         await this.userService.createItem({
             numberRegister: auth.numberRegister,
             password: newPassword,
+            password: passwordFacade.hash(auth.password),
             username: auth.username,
             type: auth.type,
             name: auth.name,
@@ -48,6 +50,7 @@ export class AuthServiceImpl implements AuthService {
     async signin(auth: authDTO): Promise<tokenDTO> {
         const user = await this.userService.getByNumberRegister(auth.numberRegister);
         if (!user || ! await AuthHelpers.verify(auth.password, user.password)) {
+        if (!user || !passwordFacade.compare(auth.password, user.password)) {
            throw new Error("numberRegister or password doesn't match");
         }
 
@@ -55,6 +58,7 @@ export class AuthServiceImpl implements AuthService {
             subject: user.id,
             expiresIn: '1d'
         });
+        const simpleUser = JSON.parse(JSON.stringify(user));
 
         return {
             token
