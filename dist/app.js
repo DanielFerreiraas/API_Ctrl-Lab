cbdf8db3573a2a6e5e2b2a281c07465264bd7258
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2024-02-17 11:24:54 -0300

    push

--- dist/app.js
+++ dist/app.js
@@ -5,15 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 require("express-async-errors");
-require("./config/env/dotenv");
-require("./config/database/typeorm");
-require("./config/container/tsyringe");
+require("./src/config/env/dotenv");
+require("./src/config/database/typeorm");
+require("./src/config/container/tsyringe");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const routes_1 = __importDefault(require("@/shared/api/routes"));
+const routes_1 = __importDefault(require("./src/shared/api/routes"));
+const multer_1 = require("./src/config/multer/multer");
+const path_1 = __importDefault(require("path"));
+const multerUser_1 = require("@/config/multer/multerUser");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.put("/pictureUser/:id", multer_1.upload.single("file"), multerUser_1.updateFileUser);
+app.put("/upload/:id", multer_1.upload.single("file"), multer_1.updateFileInDatabase);
+app.use('/uploads', express_1.default.static(path_1.default.resolve(__dirname, 'uploads')));
 app.use('/api', routes_1.default);
 app.use((err, req, res, next) => {
     console.error(err.stack);
